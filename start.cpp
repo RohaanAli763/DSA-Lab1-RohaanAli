eb48cd6e875e0c201975a920e11834ad5edb6199
Rohaan Ali <rohaanali763@gmail.com>
2025-09-15 12:16:25 +0500

    second commit

--- start.cpp
+++ start.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    cout << "Welcome to DSA Lab 1! " << endl;
-    return 0;
+    string str, pattern;
+    bool found = false;
+    cout << "Enter a string: ";
+    getline(cin, str);
+    cout << "Enter pattern to search: ";
+    getline(cin, pattern);
+    if (!str.empty())
+    {
+        int size = str.size(), psize = pattern.size();
+
+        for (int i = 0; i < size - psize + 1; i++)
+        {
+            for (int j = 0; j < psize; j++)
+            {
+                if (str[i + j] != pattern[j])
+                    break;
+                if (j == psize - 1)
+                {
+                    cout << "Pattern found at index " << i << endl;
+                    found = true;
+                }
+            }
+        }
+
+        if (!found)
+            cout << "Pattern not found";
+    }
+    else
+    {
+        cout << "String is empty";
+    }
 }
